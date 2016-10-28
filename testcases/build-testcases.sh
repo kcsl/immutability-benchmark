6aa710f223f2e6e4209d6fa96cc41d35e769eedf
Ben Holland <benjholla@gmail.com>
2016-10-28 13:13:40 -0500

    fixed build script and added compiled test cases

--- testcases/build-testcases.sh
+++ testcases/build-testcases.sh
@@ -3,20 +3,20 @@
 WD=$(pwd)
 TESTCASEBASE="$WD/TestcaseBase/src/annotations/"
 
-for D in ./source/*; 
+for TESTCATEGORY in $WD/source/*; 
 do
-    cd $WD;
-    cd $D;
+    cd $TESTCATEGORY
     for TESTCASE in *; 
     do
         echo "Building: $TESTCASE"
         BUILD="$WD/binary/$TESTCASE"
-        mkdir -p "$BUILD/src/annotations"
-        find $TESTCASEBASE -name \*.java -exec cp {} "$BUILD/src/annotations/" \;
-        mkdir -p "$BUILD/src/testcase"
-        find $TESTCASE -name \*.java -exec cp {} "$BUILD/src/testcase/" \;
+        SRC="$BUILD/src"
+        mkdir -p "$SRC/annotations"
+        find $TESTCASEBASE -name \*.java -exec cp {} "$SRC/annotations/" \;
+        mkdir -p "$SRC/testcase"
+        find $TESTCASE -name \*.java -exec cp {} "$SRC/testcase/" \;
         SOURCE=""
-        for J in `find $BUILD/src -name \*.java`
+        for J in `find $SRC -name \*.java`
         do
             SOURCE="$SOURCE $J"
         done
@@ -24,18 +24,23 @@ do
         mkdir -p "$TEMP"
         /usr/bin/javac -d "$TEMP" $SOURCE
         
-        # manually create manifest file
-        #mkdir -p "$TEMP/META-INF"
-        #MANIFEST="$TEMP/META-INF/MANIFEST.MF"
-        #echo "Manifest-Version: 1.0" >> $MANIFEST
-        #echo "Class-Path: ." >> $MANIFEST
-        #echo "Main-Class: testcase.$TESTCASE" >> $MANIFEST
+        # create a manifest file
+        MANIFEST="$BUILD/MANIFEST.MF"
+        echo "Manifest-Version: 1.0" >> $MANIFEST
+        echo "Class-Path: ." >> $MANIFEST
+        echo "Main-Class: testcase.$TESTCASE" >> $MANIFEST
         
-        # let jar utility create manifest
-        /usr/bin/jar cfe "$TESTCASE.jar" "testcase.$TESTCASE" "$TESTCASE/*.class"
-
-        # cleanup temp directory
-        rm -rf $TEMP
+        # create the executable jar
+        cd $TEMP
+        CATEGORY=$(basename $TESTCATEGORY)
+        mkdir -p "$WD/binary/$CATEGORY"
+        /usr/bin/jar cmf $MANIFEST "$WD/binary/$CATEGORY/$TESTCASE.jar" "annotations" "testcase"
+        
+        # remove build directory
+        rm -rf $BUILD
+        
+        # reset paths
+        cd $TESTCATEGORY
     done
 done
 
