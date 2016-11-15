0ba1058b193b6ad7f84795182f413d937e108923
Ben Holland <benjholla@gmail.com>
2016-11-15 16:15:03 -0600

    changing test case format (probably all need to change)

--- testcases/source/AGT/AGT101_NullLiteral_ObjectInstanceVariable/src/testcase/AGT101_NullLiteral_ObjectInstanceVariable.java
+++ testcases/source/AGT/AGT101_NullLiteral_ObjectInstanceVariable/src/testcase/AGT101_NullLiteral_ObjectInstanceVariable.java
@@ -5,18 +5,25 @@ import annotations.MUTABLE;
 public class AGT101_NullLiteral_ObjectInstanceVariable {
 
 	@MUTABLE
-	public Object f = new Object();
-
-	@Override
-	public String toString() {
-		return "AGT101_NullLiteral_ObjectInstanceVariable [f=" + f + "]";
-	}
+	public Test test = new Test();
 	
 	public static void main(String[] args) {
-		AGT101_NullLiteral_ObjectInstanceVariable test = new AGT101_NullLiteral_ObjectInstanceVariable();
+		new AGT101_NullLiteral_ObjectInstanceVariable().foo();
+	}
+	
+	public void foo(){
 		System.out.println(test);
 		test.f = null;
 		System.out.println(test);
 	}
 
+}
+
+class Test {
+	public Object f = new Object();
+	
+	@Override
+	public String toString() {
+		return "AGT101_NullLiteral_ObjectInstanceVariable [f=" + f + "]";
+	}
 }
