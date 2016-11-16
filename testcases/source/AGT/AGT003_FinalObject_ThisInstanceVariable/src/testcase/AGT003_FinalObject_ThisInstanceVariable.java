cf13c389470149a4a56a8ef2e5041aae39181fd0
benjholla <benjholla@gmail.com>
2016-11-16 13:44:10 -0600

    refactored assignment to this instance variable pattern

--- testcases/source/AGT/AGT003_FinalObject_ThisInstanceVariable/src/testcase/AGT003_FinalObject_ThisInstanceVariable.java
+++ testcases/source/AGT/AGT003_FinalObject_ThisInstanceVariable/src/testcase/AGT003_FinalObject_ThisInstanceVariable.java
@@ -5,22 +5,23 @@ import annotations.MUTABLE;
 public class AGT003_FinalObject_ThisInstanceVariable {
 
 	@MUTABLE
-	public Object f = new Object();
+	public static AGT003_FinalObject_ThisInstanceVariable test = new AGT003_FinalObject_ThisInstanceVariable();
 	
+	public Object f = new Object();
+
 	@Override
 	public String toString() {
 		return "AGT003_FinalObject_ThisInstanceVariable [f=" + f + "]";
 	}
 
 	public void foo(){
-		final Object o2 = new Object();
+		final Object o = new Object();
 		System.out.println(this.toString());
-		this.f = o2;
+		this.f = o;
 		System.out.println(this.toString());
 	}
 	
 	public static void main(String[] args) {
-		AGT003_FinalObject_ThisInstanceVariable test = new AGT003_FinalObject_ThisInstanceVariable();
 		test.foo();
 	}
 
