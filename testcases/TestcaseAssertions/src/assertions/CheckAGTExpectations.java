90d5903cd32880dd0fc86e0e71f9486fe6152d6e
benjholla <benjholla@gmail.com>
2016-11-17 14:42:54 -0600

    renamed class

--- testcases/TestcaseAssertions/src/assertions/CheckAGTExpectations.java
+++ testcases/TestcaseAssertions/src/assertions/CheckAGTExpectations.java
@@ -1,10 +1,11 @@
+package assertions;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class CheckExpectations {
+public class CheckAGTExpectations {
 
 	public static void main(String[] args){
 		String testBinary = args[0];
